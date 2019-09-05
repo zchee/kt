67ecaba6c192fb74eb6233dd2691706e16298ae5
Koichi Shiraishi <zchee.io@gmail.com>
2019-09-05 21:33:40 +0900

    signalcontext: change to stop first signal

--- pkg/signalcontext/signalcontext.go
+++ pkg/signalcontext/signalcontext.go
@@ -14,23 +14,17 @@ import (
 	errors "golang.org/x/xerrors"
 )
 
-var onlyOneSignalHandler = make(chan struct{})
-
 // SetupSignalHandler registered for SIGTERM and SIGINT. A stop channel is returned
 // which is closed on one of these signals. If a second signal is caught, the program
 // is terminated with exit code 1.
 func SetupSignalHandler() (stopCh <-chan struct{}) {
-	close(onlyOneSignalHandler) // panics when called twice
-
 	stop := make(chan struct{})
-	c := make(chan os.Signal, 2)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, shutdownSignals...)
 
 	go func() {
 		<-c
 		close(stop)
-		<-c
-		os.Exit(1) // second signal. exit directly.
 	}()
 
 	return stop
