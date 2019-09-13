c7c331df62549b67abdf2a1145c7c5f6d11fe494
Koichi Shiraishi <zchee.io@gmail.com>
2019-09-14 05:58:14 +0900

    cmd/command: add version.go

---
+++ cmd/command/version.go
@@ -0,0 +1,23 @@
+// Copyright 2019 The kt Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package command
+
+var (
+	// version indicates which version of the binary is running.
+	version = "dev"
+
+	// GitCommit indicates which git hash the binary was built off of.
+	gitCommit = ""
+)
+
+// Version is the current spinctl version.
+func Version() string {
+	v := version
+	if gitCommit != "" {
+		v += "@" + gitCommit
+	}
+
+	return v
+}
