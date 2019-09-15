124ce8a35318741464fc4f7dc3cb27337adaab46
Koichi Shiraishi <zchee.io@gmail.com>
2019-09-16 03:56:02 +0900

    io: fix package top level synopsis

--- pkg/io/doc.go
+++ pkg/io/doc.go
@@ -14,6 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package options contains flags which can be added to you command, bound, completed, and produce
+// Package io contains flags which can be added to you command, bound, completed, and produce
 // useful helper functions.  Nothing in this package can depend on kube/kube
 package io
