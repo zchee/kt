b0b57dad32537445ef657df9cb6b11738b9eb776
Koichi Shiraishi <zchee.io@gmail.com>
2019-12-04 00:33:29 +0900

    pkg/cmd: add doc.go

---
+++ pkg/cmd/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2019 The kt Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package cmd provides the kt commands.
+package cmd
